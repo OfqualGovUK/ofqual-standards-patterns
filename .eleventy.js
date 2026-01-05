7e5d1d4abb747d7d8c811b41fa8f01e6c3e8de75
PMcAllister-govuk <philip.mcallister@ofqual.gov.uk>
2026-01-05 12:18:38 +0000

    9610470 WIP: Injecting GOV.UK frontend styling to MDX files, but have broken header and footer

--- .eleventy.js
+++ .eleventy.js
@@ -1,6 +1,8 @@
+
+// .eleventy.js
 import { govukEleventyPlugin } from "@x-govuk/govuk-eleventy-plugin";
-import {DateTime} from "luxon";
-import { pathToFileURL } from 'url';
+import { DateTime } from "luxon";
+import { pathToFileURL } from "url";
 import childProcess from "child_process";
 import path from "path";
 import fs from "fs";
@@ -9,204 +11,230 @@ import { evaluate } from "@mdx-js/mdx";
 import { renderToStaticMarkup } from "react-dom/server";
 import * as runtime from "react/jsx-runtime";
 
+// NEW: rehype plugins to customize MDX output
+import rehypeAddClasses from "rehype-add-classes";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+
 function injectGitSha(eleventyConfig, gitHubRepositoryUrl) {
-    let latestGitCommitHash = process.env.GITHUB_COMMIT_SHA;
+  let latestGitCommitHash = process.env.GITHUB_COMMIT_SHA;
 
-    if(!latestGitCommitHash) {
-        try {
-            latestGitCommitHash = childProcess.execSync('git rev-parse HEAD').toString().trim();
-        } catch (e) {
-            console.warn("Unable to determine current Git commit hash. Permalinks will not be generated.")
-            return;
-        }
+  if (!latestGitCommitHash) {
+    try {
+      latestGitCommitHash = childProcess.execSync("git rev-parse HEAD").toString().trim();
+    } catch (e) {
+      console.warn("Unable to determine current Git commit hash. Permalinks will not be generated.");
+      return;
     }
+  }
 
-    eleventyConfig.addGlobalData(
-        'gitHashPath',
-        gitHubRepositoryUrl + '/blob/' + latestGitCommitHash + '/docs'
-    );
+  eleventyConfig.addGlobalData("gitHashPath", gitHubRepositoryUrl + "/blob/" + latestGitCommitHash + "/docs");
 }
 
-export default async function(eleventyConfig) {
-    const _siteRoot = process.env.SITE_ROOT ?? 'http://localhost:8080/';
-    const gitHubRepositoryUrl = "https://github.com/OfqualGovUK/ofqual-standards-patterns";
+export default async function (eleventyConfig) {
+  const _siteRoot = process.env.SITE_ROOT ?? "http://localhost:8080/";
+  const gitHubRepositoryUrl = "https://github.com/OfqualGovUK/ofqual-standards-patterns";
 
-    // Pass assets through to final build directory
-    eleventyConfig.addPassthroughCopy({ "docs/assets/logos": "assets/logos"});
-    eleventyConfig.addPassthroughCopy({ "docs/assets/images": "assets/images"});
-    eleventyConfig.addPassthroughCopy({ "docs/ixpatterns/examples": "assets/examples"});
-    
-    // Register the plugins
-    let govukPluginOptions = {
-        opengraphImageUrl: '/assets/logos/govuk-opengraph-image.png',
-        homeKey: 'Home',
-        header: {
-            logotype: {
-                html:
-                    '<span class="govuk-header__logotype">' +
-                    '  <img src="/assets/logos/OfqualLogoWhite.svg" height="64px" alt="Ofqual Logo">' +
-                    '  <img class="hide" src="/assets/logos/OfqualLogoBlack.svg" height="64px" alt="Ofqual Logo">' +
-                    '</span>'
+  // Pass assets through to final build directory
+  eleventyConfig.addPassthroughCopy({ "docs/assets/logos": "assets/logos" });
+  eleventyConfig.addPassthroughCopy({ "docs/assets/images": "assets/images" });
+  eleventyConfig.addPassthroughCopy({ "docs/ixpatterns/examples": "assets/examples" });
+
+  // Register the GOV.UK plugin
+  let govukPluginOptions = {
+    opengraphImageUrl: "/assets/logos/govuk-opengraph-image.png",
+    homeKey: "Home",
+    header: {
+      logotype: {
+        html:
+          '&lt;span class="govuk-header__logotype"&gt;' +
+          '  &lt;img src="/assets/logos/OfqualLogoWhite.svg" height="64px" alt="Ofqual Logo"&gt;' +
+          '  &lt;img class="hide" src="/assets/logos/OfqualLogoBlack.svg" height="64px" alt="Ofqual Logo"&gt;' +
+          "&lt;/span&gt;",
+      },
+      productName: "Ofqual Digital and Data",
+      search: {
+        label: "Search site",
+        indexPath: "/search-index.json",
+        sitemapPath: "/sitemap/",
+      },
+    },
+    footer: {
+      copyright: {
+        html:
+          '&lt;a class="govuk-footer__link govuk-footer__copyright-logo" href="https://www.nationalarchives.gov.uk/information-management/re-using-public-sector-information/uk-government-licensing-framework/crown-copyright/"&gt;©&amp;nbsp;Crown copyright&lt;/a&gt;',
+      },
+      meta: {
+        items: [
+          { href: "/about/", text: "About" },
+          { href: "/cookies/", text: "Cookies" },
+          { href: "/accessibility-statement/", text: "Accessibility" },
+          { href: gitHubRepositoryUrl, text: "GitHub repository" },
+        ],
+      },
+    },
+    stylesheets: ["/styles/base.css"],
+    templates: {
+      searchIndex: true,
+    },
+  };
+  eleventyConfig.addPlugin(govukEleventyPlugin, govukPluginOptions);
+
+  // Enable `.mdx` files as a template format
+  eleventyConfig.addTemplateFormats("mdx");
+
+  // GOV.UK class map applied to MDX via rehype-add-classes
+  const govukClassMap = {
+    // Typography
+    p: "govuk-body",
+    "h1,h2,h3,h4,h5,h6": "govuk-heading",
+    h1: "govuk-heading-xl",
+    h2: "govuk-heading-l",
+    h3: "govuk-heading-m",
+    h4: "govuk-heading-s",
+
+    // Lists
+    ul: "govuk-list govuk-list--bullet",
+    ol: "govuk-list govuk-list--number",
+
+    // Links
+    a: "govuk-link",
+
+    // Optional: code blocks
+    pre: "govuk-pre",
+    code: "govuk-code",
+  };
+
+  // MDX with full front‑matter compatibility + rehype transforms
+  eleventyConfig.addExtension("mdx", {
+    compile: async (str, inputPath) => {
+      // Evaluate MDX to a React component; baseUrl must point at the file
+      const { default: MDXContent } = await evaluate(str, {
+        ...runtime,
+        baseUrl: pathToFileURL(inputPath),
+
+        // Rehype plugin to add GOV.UK styles to MDX processed files
+        rehypePlugins: [
+          [rehypeSlug], // adds id attributes to headings
+          [
+            rehypeAutolinkHeadings,
+            {
+              behavior: "append",
+              properties: { class: "govuk-heading__permalink", ariaHidden: true },
             },
-            productName: 'Ofqual Digital and Data',
-            search: {
-                label: 'Search site',
-                indexPath: '/search-index.json',
-                sitemapPath: '/sitemap/'
-            }
-        },
-        footer: {
-            copyright: {
-                html: '<a class="govuk-footer__link govuk-footer__copyright-logo" href="https://www.nationalarchives.gov.uk/information-management/re-using-public-sector-information/uk-government-licensing-framework/crown-copyright/">©&nbsp;Crown copyright</a>'
-            },
-            meta: {
-                items: [
-                    {
-                        href: '/about/',
-                        text: 'About'
-                    },
-                    {
-                        href: '/cookies/',
-                        text: 'Cookies'
-                    },
-                    {
-                        href: '/accessibility-statement/',
-                        text: 'Accessibility'
-                    },
-                    {
-                        href: gitHubRepositoryUrl,
-                        text: 'GitHub repository'
-                    }
-                ]
-            }
-        },
-        stylesheets: ['/styles/base.css'],
-        templates: {
-            searchIndex: true,
-        }
-    };
-    eleventyConfig.addPlugin(govukEleventyPlugin, govukPluginOptions)
+          ],
+          [rehypeAddClasses, govukClassMap],
+        ],
+      });
 
-    // Enable `.mdx` files as a template format
-    eleventyConfig.addTemplateFormats("mdx");
+      // Eleventy calls this renderer with the merged data cascade (incl. front matter)
+      return async function (data) {
+        const element = await MDXContent(data);
+        return renderToStaticMarkup(element);
+      };
+    },
+  });
 
-    // MDX with full front‑matter compatibility
-    eleventyConfig.addExtension("mdx", {
-        compile: async (str, inputPath) => {
-            // Evaluate MDX to a React component; baseUrl must point at the file
-            const { default: MDXContent } = await evaluate(str, {
-                ...runtime,
-                baseUrl: pathToFileURL(inputPath),
-            });
-
-            // Eleventy calls this renderer with the merged data cascade (incl. front matter)
-            return async function (data) {
-                const element = await MDXContent(data);
-                return renderToStaticMarkup(element);
-            };
-        },
-    });
-    
     // Customise markdown-it renderer provided by x-gov 11ty plugin. Plugin execution is
     // deferred, so this needs to be a plugin, and added after the x-gov plugin is.
-    eleventyConfig.addPlugin((eleventyConfig) => {
-        eleventyConfig.amendLibrary('md', md => md.use(dlAsSummaryList));
-    });
+  eleventyConfig.addPlugin((eleventyConfig) => {
+    eleventyConfig.amendLibrary("md", (md) => md.use(dlAsSummaryList));
+  });
 
-    eleventyConfig.addFilter("postDate", (dateObj) => {
-        return DateTime.fromJSDate(dateObj).toFormat('d MMMM yyyy');
-    });
+  eleventyConfig.addFilter("postDate", (dateObj) => {
+    return DateTime.fromJSDate(dateObj).toFormat("d MMMM yyyy");
+  });
 
-    // Used for tag page generation
-    eleventyConfig.addFilter("getAllTags", collection => {
-      let tagSet = new Set();
-      for(let item of collection) {
-          (item.data.tags || []).forEach(tag => tagSet.add(tag));
-      }
-      return Array.from(tagSet).sort(function(a, b) {
-        return a.localeCompare(b); // sort by tag name
-      });
-    });
-
-    eleventyConfig.addFilter("filterTagList", function filterTagList(tags) {
-        // Tags in array are ignored, no tag list page is generated for these
-        return (tags || []).filter(tag => ["homepage"].indexOf(tag) === -1);
-    });
-
-    eleventyConfig.addFilter("orderPagesByTitle", function orderByTitle(collection) {
-      return collection.sort(function(a, b) {
-        return a.data.title.localeCompare(b.data.title); // sort by title ascending
-      });
-    });
-
-    await Promise.all(
-    fs.readdirSync(path.join(process.cwd(), 'lib', 'filters'))
-        .map(file => path.parse(file))
-        .filter(({ ext }) => ext === '.js')
-        .map(async ({ name, base }) =>
-        eleventyConfig.addFilter(
-            name,
-            (await import(pathToFileURL(path.join(process.cwd(), 'lib', 'filters', base)).href)).default
-        )
-        )
-    );
-
-    eleventyConfig.addCollection("getAllStandardsOrderedByID", function(collectionApi) {
-      return collectionApi.getFilteredByGlob("**/standards/*.md").sort(function(a, b) {
-          return a.data.id.localeCompare(b.data.id); // sort by ID ascending
-        });
-    });
-
-    eleventyConfig.addCollection("getAllStandardsOrderedByTitle", function(collectionApi) {
-      return collectionApi.getFilteredByGlob("**/standards/*.md").sort(function(a, b) {
-          return a.data.title.localeCompare(b.data.title); // sort by title ascending
-        });
-    });
-
-    eleventyConfig.addCollection("getAllPrinciplesOrderedByTitle", function(collectionApi) {
-      return collectionApi.getFilteredByGlob("**/principles/*.md").sort(function(a, b) {
-        return a.data.title.localeCompare(b.data.title); // sort by title ascending
-      });
-    });
-
-    eleventyConfig.addCollection("getAllPatternsOrderedByTitle", function(collectionApi) {
-      return collectionApi.getFilteredByGlob("**/patterns/*.md").sort(function(a, b) {
-        return a.data.title.localeCompare(b.data.title); // sort by title ascending
-      });
-    });
-
-    eleventyConfig.addCollection("getAllIXPatternsOrderedByTitle", function(collectionApi) {
-        return collectionApi.getFilteredByGlob("**/ixpatterns/*.{md,mdx}").sort(function(a, b) {
-            return a.data.title.localeCompare(b.data.title); // sort by title ascending
-        });
-    });
-
-    eleventyConfig.addGlobalData("phaseBannerConfiguration", () => {
-      return {
-        tag: {
-          text: "New Service"
-        },
-        html: 'This is a new service – your ' +
-            '<a class="govuk-link" href="/provide-feedback/" target="_blank" rel="noopener">' +
-            'feedback (opens in a new tab)' +
-            '</a> ' +
-            'will help us to improve it.'
-      }
-    });
-
-    eleventyConfig.addGlobalData('siteRoot', _siteRoot);
-
-    injectGitSha(eleventyConfig, gitHubRepositoryUrl);
-
-    return {
-        dataTemplateEngine: 'njk',
-        htmlTemplateEngine: 'njk',
-        markdownTemplateEngine: 'njk',
-        dir: {
-            data: '../_data',
-            layouts: '../_includes/layouts',
-            includes: '../_includes',
-            input: 'docs'
-        }
+  // Used for tag page generation
+  eleventyConfig.addFilter("getAllTags", (collection) => {
+    let tagSet = new Set();
+    for (let item of collection) {
+      (item.data.tags || []).forEach((tag) => tagSet.add(tag));
     }
-};
+    return Array.from(tagSet).sort(function (a, b) {
+      return a.localeCompare(b); // sort by tag name
+    });
+  });
+
+  eleventyConfig.addFilter("filterTagList", function filterTagList(tags) {
+    // Tags in array are ignored, no tag list page is generated for these
+    return (tags || []).filter((tag) => ["homepage"].indexOf(tag) === -1);
+  });
+
+  eleventyConfig.addFilter("orderPagesByTitle", function orderByTitle(collection) {
+    return collection.sort(function (a, b) {
+      return a.data.title.localeCompare(b.data.title); // sort by title ascending
+    });
+  });
+
+  await Promise.all(
+    fs
+      .readdirSync(path.join(process.cwd(), "lib", "filters"))
+      .map((file) => path.parse(file))
+      .filter(({ ext }) => ext === ".js")
+      .map(async ({ name, base }) =>
+        eleventyConfig.addFilter(
+          name,
+          (await import(pathToFileURL(path.join(process.cwd(), "lib", "filters", base)).href)).default
+        )
+      )
+  );
+
+  eleventyConfig.addCollection("getAllStandardsOrderedByID", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("**/standards/*.md").sort(function (a, b) {
+      return a.data.id.localeCompare(b.data.id); // sort by ID ascending
+    });
+  });
+
+  eleventyConfig.addCollection("getAllStandardsOrderedByTitle", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("**/standards/*.md").sort(function (a, b) {
+      return a.data.title.localeCompare(b.data.title); // sort by title ascending
+    });
+  });
+
+  eleventyConfig.addCollection("getAllPrinciplesOrderedByTitle", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("**/principles/*.md").sort(function (a, b) {
+      return a.data.title.localeCompare(b.data.title); // sort by title ascending
+    });
+  });
+
+  eleventyConfig.addCollection("getAllPatternsOrderedByTitle", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("**/patterns/*.md").sort(function (a, b) {
+      return a.data.title.localeCompare(b.data.title); // sort by title ascending
+    });
+  });
+
+  eleventyConfig.addCollection("getAllIXPatternsOrderedByTitle", function (collectionApi) {
+    return collectionApi.getFilteredByGlob("**/ixpatterns/*.{md,mdx}").sort(function (a, b) {
+      return a.data.title.localeCompare(b.data.title); // sort by title ascending
+    });
+  });
+
+  eleventyConfig.addGlobalData("phaseBannerConfiguration", () => {
+    return {
+      tag: { text: "New Service" },
+      html:
+        'This is a new service – your ' +
+        '&lt;a class="govuk-link" href="/provide-feedback/" target="_blank" rel="noopener"&gt;' +
+        "feedback (opens in a new tab)" +
+        "&lt;/a&gt; " +
+        "will help us to improve it.",
+    };
+  });
+
+  eleventyConfig.addGlobalData("siteRoot", _siteRoot);
+
+  injectGitSha(eleventyConfig, gitHubRepositoryUrl);
+
+  return {
+    dataTemplateEngine: "njk",
+    htmlTemplateEngine: "njk",
+    markdownTemplateEngine: "njk",
+    dir: {
+      data: "../_data",
+      layouts: "../_includes/layouts",
+      includes: "../_includes",
+      input: "docs",
+    },
+  };
+}
